00b473bec4b447d3af42274224e073f294111fa0
dangehub <dangehub@users.noreply.github.com>
2026-01-18 18:37:44 +0000

    [create-pull-request] automated change

--- src/utils/Log.ts
+++ src/utils/Log.ts
@@ -4,7 +4,7 @@ import { TemplaterError } from "./Error";
 export function log_update(msg: string): void {
     const notice = new Notice("", 15000);
     const messageEl = createFragment((frag) => {
-        frag.createEl("b", { text: "Templater update" });
+        frag.createEl("b", { text: this.t("Templater update") });
         frag.createEl("span", { text: ":" });
         frag.createEl("br");
         frag.createEl("span", { text: msg });
@@ -15,14 +15,14 @@ export function log_update(msg: string): void {
 export function log_error(e: Error | TemplaterError): void {
     const notice = new Notice("", 8000);
     const messageEl = createFragment((frag) => {
-        frag.createEl("b", { text: "Templater Error" });
+        frag.createEl("b", { text: this.t("Templater Error") });
         frag.createEl("span", { text: ":" });
         frag.createEl("br");
         frag.createEl("span", { text: e.message });
         if (e instanceof TemplaterError && e.console_msg) {
             frag.createEl("br");
             frag.createEl("span", {
-                text: "Check console for more information",
+                text: this.t("Check console for more information"),
             });
             console.error(`Templater Error:`, e.message, "\n", e.console_msg);
         }
